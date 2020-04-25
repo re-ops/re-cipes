5f2d46ee93fa2d85568d3e27b1c64b9c111aa381
ronen <narkisr@gmail.com>
2020-04-25 22:25:57 +1000

    version bump

--- main/re_cipes/main.clj
+++ main/re_cipes/main.clj
@@ -45,7 +45,7 @@
     :app {
       :command     "re-cipes"
       :description "Re-cipes provisioning cli"
-      :version     "0.1.2"
+      :version     "0.1.3"
     }
     :commands [
       {:command "provision" :short "prov"
