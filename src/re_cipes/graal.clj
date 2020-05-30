3cb76ca5d678c96dbbb9d63e8bf8e96d495d96be
ronen <narkisr@gmail.com>
2020-05-30 18:42:04 +1000

    Latest graalvm version

--- src/re_cipes/graal.clj
+++ src/re_cipes/graal.clj
@@ -18,11 +18,11 @@
             (fn []
               (script (~bin "install" ~pkg))))]
     (let [{:keys [home]} (configuration)
-          version "20.0.0"
+          version "20.1.0"
           release (<< "graalvm-ce-java8-linux-amd64-~{version}")
           dest (<< "graalvm-ce-java8-~{version}")
           tmp (<< "/tmp/~{release}.tar.gz")
-          expected "16ef8d89f014b4d295b7ca0c54343eab3c7d24e18b2d376665f5b12bb643723d"
+          expected "4fac212b37cd548831fd6587dd4d59dc068068815aa20323b47fde9529d6bb6e"
           url (<< "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-~{version}/~{release}.tar.gz")]
       (download url tmp expected)
       (untar tmp "/opt/")
