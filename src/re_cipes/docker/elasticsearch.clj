79be41bb5bf369e42047010d422d3a0d59b449b5
ronen <narkisr@gmail.com>
2020-07-04 18:26:31 +1000

    Volume setup for ELK and Elasticsearch

--- src/re_cipes/docker/elasticsearch.clj
+++ src/re_cipes/docker/elasticsearch.clj
@@ -4,18 +4,23 @@
    [re-cog.resources.permissions :refer (set-file-acl)]
    [re-cipes.docker.server]
    [re-cog.resources.service :refer (on-boot)]
-   [re-cog.resources.file :refer (copy directory)]
+   [re-cog.resources.file :refer (copy directory chmod)]
    [re-cog.common.recipe :refer (require-recipe)]))
 
 (require-recipe)
 
-(def-inline {:depends #'re-cipes.docker.server/services} get-source
+(def-inline volume
+  "Setting up data volume"
+  []
+  (set-file-acl "re-ops" "rwX" "/var/")
+  (directory "/var/data" :present)
+  (chmod "/var/data" "a+wrx"))
+
+(def-inline {:depends [#'re-cipes.docker.server/services re-cipes.docker.elasticsearch/volume]} get-source
   "Grabbing source"
   []
   (let [repo "https://github.com/re-ops/re-dock.git"
         dest "/etc/docker/compose/elasticsearch"]
     (clone repo dest)
-    (set-file-acl "re-ops" "rwX" "/var/")
-    (directory "/var/data" :present)
     (copy (<< "~{dest}/elasticsearch.yml") (<< "~{dest}/docker-compose.yml"))
     (on-boot "docker-compose@elasticsearch" :enable)))
--- src/re_cipes/docker/elk_stack.clj
+++ src/re_cipes/docker/elk_stack.clj
@@ -1,13 +1,24 @@
 (ns re-cipes.docker.elk-stack
   "Dockerized ELK OSS full stack"
   (:require
+   [re-cog.resources.permissions :refer (set-file-acl)]
+   [re-cog.resources.file :refer (directory chmod)]
    [re-cog.resources.service :refer (on-boot)]
    [re-cipes.docker.server]
    [re-cog.common.recipe :refer (require-recipe)]))
 
 (require-recipe)
 
-(def-inline {:depends #'re-cipes.docker.server/services} get-source
+(def-inline volume
+  "Setting up data volume"
+  []
+  (set-file-acl "re-ops" "rwX" "/var/")
+  (directory "/var/data" :present)
+  (directory "/var/data/elasticsearch" :present)
+  (directory "/var/data/grafana" :present)
+  (chmod "/var/data" "a+wrx" {:recursive true}))
+
+(def-inline {:depends [#'re-cipes.docker.server/services #'re-cipes.docker.elk-stack/volume]} get-source
   "Grabbing source"
   []
   (let [repo "https://github.com/re-ops/re-dock.git"
