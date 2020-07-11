d73a2a541528e7143d3196e5f84d8fdc47406612
ronen <narkisr@gmail.com>
2020-07-11 20:31:24 +1000

    Bumping version adding Grafana

--- main/re_cipes/main.clj
+++ main/re_cipes/main.clj
@@ -45,7 +45,7 @@
     :app {
       :command     "re-cipes"
       :description "Re-cipes provisioning cli"
-      :version     "0.1.25"
+      :version     "0.1.26"
     }
     :commands [
       {:command "provision" :short "prov"
