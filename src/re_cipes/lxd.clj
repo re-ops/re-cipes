42872a1fcffd492dedcbbea4d0cb984e701bb395
ronen <narkisr@gmail.com>
2020-08-03 01:07:56 +1000

    Networking and remove fix

--- src/re_cipes/lxd.clj
+++ src/re_cipes/lxd.clj
@@ -1,6 +1,7 @@
 (ns re-cipes.lxd
   "Setting up lxd locally and adding it to the local lxc client"
   (:require
+   [re-cog.resources.ufw :refer (add-interface enable-forwarding)]
    [re-cog.facts.config :refer (configuration)]
    [re-cog.resources.file :refer (template)]
    [re-cog.common.recipe :refer (require-recipe)]
@@ -12,19 +13,22 @@
   "Installing lxd"
   []
   (let [{:keys [lxd]} (configuration)]
-    (letfn [(remove- [bind]
-              (fn []
-                (script (pipe ("/usr/bin/lxd" "remove" ~bind) "true"))))
-            (init []
-                  (script
-                   (pipe
-                    ("/usr/bin/cat" "/tmp/preseed.yaml")
-                    ("/usr/bin/lxd" "init" "--preseed"))))]
+    (letfn [(init []
+              (script
+               (pipe
+                ("/usr/bin/cat" "/tmp/preseed.yaml")
+                ("/usr/bin/lxd" "init" "--preseed"))))]
       (package "lxd" :present)
       (package "zfsutils-linux" :present)
       (template "/tmp/resources/templates/lxd/preseed.mustache" "/tmp/preseed.yaml" lxd)
-      (run (remove- (lxd :bind)))
       (run init))))
+
+(def-inline bridging
+  "Enable bridge networking"
+  []
+  (add-interface "lxdbr0" :in :allow)
+  (add-interface "lxdbr0" :out :allow)
+  (enable-forwarding))
 
 (def-inline {:depends #'re-cipes.lxd/server} remote
   "add local lxd instance locally"
@@ -37,12 +41,16 @@
         key (<< "~{root}/client.key")
         crt (<< "~{root}/client.crt")
         pass (<< "pass:~{password}")]
-    (letfn [(add-remote [password]
+    (letfn [(remove-remote [bind]
               (fn []
-                (script
-                 ("/usr/bin/lxc" "remote" "add" "127.0.0.1" "--password" ~password "--accept-certificate"))))
+                (script (pipe ("/usr/bin/lxc" "remove" ~bind) "true"))))
+            (add-remote [password]
+                        (fn []
+                          (script
+                           ("/usr/bin/lxc" "remote" "add" "127.0.0.1" "--password" ~password "--accept-certificate"))))
             (export []
                     (script
                      ("/usr/bin/openssl" "pkcs12" "-export" "-out" ~out "-inkey" ~key "-in" ~crt "-certfile" ~certfile "-passout" ~pass)))]
+      (run (remove-remote (lxd :bind)))
       (run (add-remote password))
       (run export))))
