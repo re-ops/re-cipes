bbcb1d0f3d6ca33be252ddf1ed83e72700f263a5
ronen <narkisr@gmail.com>
2020-08-04 23:20:14 +1000

    fixing bridging and passing lxd to script functions

--- src/re_cipes/lxd.clj
+++ src/re_cipes/lxd.clj
@@ -1,6 +1,7 @@
 (ns re-cipes.lxd
   "Setting up lxd locally and adding it to the local lxc client"
   (:require
+   re-cipes.hardening
    [re-cog.resources.ufw :refer (add-interface enable-forwarding)]
    [re-cog.facts.config :refer (configuration)]
    [re-cog.resources.file :refer (template)]
@@ -23,7 +24,7 @@
       (template "/tmp/resources/templates/lxd/preseed.mustache" "/tmp/preseed.yaml" lxd)
       (run init))))
 
-(def-inline bridging
+(def-inline {:depends #'re-cipes.hardening/firewall} bridging
   "Enable bridge networking"
   []
   (add-interface "lxdbr0" :in :allow)
@@ -34,23 +35,22 @@
   "add local lxd instance locally"
   []
   (let [{:keys [lxd home]} (configuration)
-        {:keys [password]} lxd
         root (<< "~{home}/snap/lxd/current/.config/lxc")
         certfile (<< "~{root}/servercerts/127.0.0.1.crt")
         out (<< "~{root}/certificate.p12")
         key (<< "~{root}/client.key")
         crt (<< "~{root}/client.crt")
-        pass (<< "pass:~{password}")]
-    (letfn [(remove-remote [bind]
+        pass (<< "pass:~(lxd :password)")]
+    (letfn [(remove-remote [{:keys [bind]}]
               (fn []
-                (script (pipe ("/usr/bin/lxc" "remove" ~bind) "true"))))
-            (add-remote [password]
+                (script (pipe ("/usr/bin/lxc" "remote" "remove" ~bind) "true"))))
+            (add-remote [{:keys [password bind]}]
                         (fn []
                           (script
-                           ("/usr/bin/lxc" "remote" "add" "127.0.0.1" "--password" ~password "--accept-certificate"))))
+                           ("/usr/bin/lxc" "remote" "add" ~bind "--password" ~password "--accept-certificate"))))
             (export []
                     (script
                      ("/usr/bin/openssl" "pkcs12" "-export" "-out" ~out "-inkey" ~key "-in" ~crt "-certfile" ~certfile "-passout" ~pass)))]
-      (run (remove-remote (lxd :bind)))
-      (run (add-remote password))
+      (run (remove-remote lxd))
+      (run (add-remote lxd))
       (run export))))
