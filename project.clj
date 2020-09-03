77d6452e3c6562f7b064fdc5c3fcd752d164d6c4
ronen <narkisr@gmail.com>
2020-09-03 23:11:59 +1000

    Using fixed cog

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject re-cipes "0.1.31"
+(defproject re-cipes "0.1.32"
   :description "Provioning re-cipes for Linux machines"
   :url "https://github.com/re-ops/re-cipes"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
       [org.clojure/clojure "1.10.1"]
-      [re-cog "0.5.1"]
+      [re-cog "0.5.2"]
   ]
 
   :plugins [
