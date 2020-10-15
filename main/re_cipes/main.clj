017fe363946e5dc4b4ff7e5130f53b90207d369a
ronen <narkisr@gmail.com>
2020-10-15 21:45:43 +1100

    bumping release

--- main/re_cipes/main.clj
+++ main/re_cipes/main.clj
@@ -47,7 +47,7 @@
     :app {
       :command     "re-cipes"
       :description "Re-cipes provisioning cli"
-      :version     "0.2.4"
+      :version     "0.2.5"
     }
     :commands [
       {:command "provision" :short "prov"
