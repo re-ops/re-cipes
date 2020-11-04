9f8c5d878e5eee15ecd541bcc5e603d3ac0c3971
ronen <narkisr@gmail.com>
2020-11-04 23:18:37 +1100

    Using non root pip install and making default script executable

--- src/re_cipes/apps/letsencrypt.clj
+++ src/re_cipes/apps/letsencrypt.clj
@@ -1,6 +1,9 @@
 (ns re-cipes.apps.letsencrypt
   "A letsencrypt client"
   (:require
+   [re-cipes.access :refer (permissions)]
+   [re-cog.facts.config :refer (configuration)]
+   [re-cog.resources.exec :refer (run)]
    [re-cog.resources.permissions :refer (set-file-acl)]
    [re-cog.resources.file :refer (copy directory chmod)]
    [re-cog.resources.download :refer (download)]
@@ -20,17 +23,21 @@
     (clone repo dest)
     (chmod "/srv/dehydrated/dehydrated" "a+wrx" {})))
 
-(def-inline {:depends #'re-cipes.apps.letsencrypt/dehydrated} dns-lexicon
+(def-inline {:depends [#'re-cipes.access/permissions #'re-cipes.apps.letsencrypt/dehydrated]} dns-lexicon
   "lexicon setup"
   []
   (letfn [(pip []
             (script
-             ("sudo" "/usr/bin/pip3" "install" "requests[security]")
-             ("sudo" "/usr/bin/pip3" "install" "dns-lexicon")))]
-    (let [hook "dehydrated.default.sh"
+             ("/usr/bin/pip3" "install" "requests[security]")
+             ("/usr/bin/pip3" "install" "dns-lexicon")))]
+    (let [{:keys [home]} (configuration)
+          hook "dehydrated.default.sh"
           url (<< "https://raw.githubusercontent.com/AnalogJ/lexicon/master/examples/~{hook}")
           dest "/srv/dehydrated"
           sum "36fc962978becb696a0f0980a08ec09827440355f6c2c520f96bd5c64831a87f"]
       (download url (<< "~{dest}/~{hook}") sum)
+      (chmod  (<< "~{dest}/~{hook}") "a+wrx" {})
       (doseq [dep ["build-essential" "python-dev" "curl" "libffi-dev" "libssl-dev" "python3-pip"]]
-        (package dep :present)))))
+        (package dep :present))
+      (run pip)
+      (symlink "/usr/local/bin/lexicon" (<< "~{home}/.local/bin/lexicon")))))
