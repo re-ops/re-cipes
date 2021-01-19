566d4befd206d80618bf8d77cddca15c659c1cca
ronen <narkisr@gmail.com>
2021-01-20 01:22:13 +1100

    adding cic hardening and changing template path

--- src/re_cipes/hardening.clj
+++ src/re_cipes/hardening.clj
@@ -3,7 +3,7 @@
   (:require
    [re-cog.resources.exec :refer [run]]
    [re-cog.common.recipe :refer (require-recipe)]
-   [re-cog.resources.file :refer (line line-set copy)]
+   [re-cog.resources.file :refer (line line-set copy template)]
    [re-cog.resources.permissions :refer (set-file-acl)]
    [re-cog.resources.service :refer (service on-boot)]
    [re-cog.resources.ufw :refer (set-state add-rule reset)]))
@@ -15,7 +15,7 @@
   []
   (package "openssh-server" :present)
   (package "rng-tools" :present)
-  (set-file-acl "re-ops" "rwX" "/etc/ssh/sshd_config")
+  (set-file-acl "re-ops" "rwx" "/etc/ssh/sshd_config")
   (line "/etc/ssh/sshd_config" "PermitRootLogin" :uncomment :with "#")
   (line-set "/etc/ssh/sshd_config" "PermitRootLogin" "no" " ")
   (line "/etc/ssh/sshd_config" "PasswordAuthentication" :uncomment :with "#")
@@ -24,14 +24,16 @@
   (line "/etc/ssh/sshd_config" "\nUseDns no" :present)
   (service "ssh" :restart))
 
-(def-inline networking
-  "Hardening network"
+(def-inline systcl-hardening
+  "Hardening network, cis using sysctl"
   []
   (letfn [(sysctl-reload []
             (script ("sudo" "/usr/sbin/sysctl" "--system")))]
-    (let [target "/etc/sysctl.d/10-network-hardening.conf"]
-      (set-file-acl "re-ops" "rwX" "/etc/sysctl.d")
-      (copy "/tmp/resources/templates/networking/harden.conf" target)
+    (let [target "/etc/sysctl.d/10-network-hardening.conf"
+          root "/tmp/resources/templates/hardening"]
+      (set-file-acl "re-ops" "rwx" "/etc/sysctl.d")
+      (copy (<< "~{root}/network.conf") target)
+      (copy (<< "~{root}/cis.conf") target)
       (run sysctl-reload))))
 
 (def-inline disable-bluetooth
@@ -47,3 +49,21 @@
   (reset)
   (add-rule 22 :allow {})
   (set-state :enable))
+
+(def-inline cis-packages
+  "CIS benchmark related settings"
+  []
+  (package "aide" :present))
+
+(def-inline disable-fs
+  "Disable loading of unused Filesystems cis 1.1.1.1-5"
+  []
+  (let [source "/tmp/resources/templates/hardening/disable-fs.mustache"
+        fs (map (fn [v] {:fs v}) ["freevxfs" "jffs2" "hfs" "hfsplus" "udf"])]
+    (template  source "/etc/modprobe.d/fs-disable.conf" {:filesystems fs})))
+
+(def-inline auditing
+  "Enabling auditd cis: 4.1.2"
+  []
+  (package "auditd" :present)
+  (on-boot "auditd" :enable))
