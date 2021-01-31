5c40fcbca08d3b4d7431ac2807bd573085c0860b
ronen <narkisr@gmail.com>
2021-01-31 14:22:53 +1100

    version bump

--- bin/binary.sh
+++ bin/binary.sh
@@ -1,2 +1,2 @@
 LEIN_SNAPSHOTS_IN_RELEASE=1 lein with-profile package do uberjar
-cat bin/stub.sh target/re-cipes-0.3.3-standalone.jar > target/re-cipes && chmod +x target/re-cipes
+cat bin/stub.sh target/re-cipes-0.3.4-standalone.jar > target/re-cipes && chmod +x target/re-cipes
--- main/re_cipes/main.clj
+++ main/re_cipes/main.clj
@@ -47,7 +47,7 @@
     :app {
       :command     "re-cipes"
       :description "Re-cipes provisioning cli"
-      :version     "0.3.3"
+      :version     "0.3.4"
     }
     :commands [
       {:command "provision" :short "prov"
