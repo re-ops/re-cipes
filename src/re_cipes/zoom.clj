d97fa0f564237f0d8a14d0a2eb59a756d7375f37
ronen <narkisr@gmail.com>
2021-02-06 18:07:10 +1100

    Updating zoom checksum

--- src/re_cipes/zoom.clj
+++ src/re_cipes/zoom.clj
@@ -11,7 +11,7 @@
   "Installing zoom"
   []
   (let [url "https://zoom.us/client/latest/zoom_amd64.deb"
-        sum "e13ac9b8b233d31b2f4015e7d187110d8d8c679c83e577b6dbeab4795a33956d"
+        sum "292bfb9f7baad32e2320949efbb6742ef1bbee3152e81a4edf61941d16e45d9f"
         dest  "/tmp/zoom_amd64.deb"]
     (download url dest sum)
     (package dest :present)))
