9c8b6a85ae4e3e17d0d8bf3b2703d51bda8aadbe
ronen <narkisr@gmail.com>
2021-05-21 20:49:51 +1000

    jdk 11 migration

--- src/re_cipes/jdk.clj
+++ src/re_cipes/jdk.clj
@@ -1,19 +1,30 @@
 (ns re-cipes.jdk
   "Setting up the jdk"
   (:require
-   [re-cog.common.recipe :refer (require-recipe)]
-   [re-cog.resources.download :refer (download)]))
+   [re-cipes.access :refer (permissions)]
+   [clojure.core.strint :refer (<<)]
+   [re-cog.resources.file :refer (file line)]
+   [re-cog.resources.package :refer (package key-file update-)]
+   [re-cog.resources.user :refer (group-add)]
+   [re-cog.resources.download :refer (download)]
+   [re-cog.common.recipe :refer (require-recipe)]))
 
 (require-recipe)
 
-(def-inline corretto
-  "Setting up Amazon corretto 8 jdk"
+(def-inline {:depends #'re-cipes.access/permissions} jdk-11
+  "Setting up Amazon corretto 11 jdk"
   []
-  (let [version "8.252.09-1"
-        url-version (.replace version "-" ".")
-        deb (<< "java-1.8.0-amazon-corretto-jdk_~{version}_amd64.deb")
-        url (<< "https://corretto.aws/downloads/resources/~{url-version}/java-1.8.0-amazon-corretto-jdk_~{version}_amd64.deb")
-        sum "bc266354a0a02aaf93ecc5062ee24ff8"]
-    (download url (<< "/tmp/~{deb}") sum :hash-type :md5)
-    (package "java-common" :present)
-    (package (<< "/tmp/~{deb}") :present)))
+  (let
+   [sources "/etc/apt/sources.list.d"
+    listing (<< "~{sources}/corretto.list")
+    keyrings "/usr/share/keyrings"
+    url "https://apt.corretto.aws/corretto.key"
+    key "corretto.key"
+    repo "deb https://apt.corretto.aws stable main"]
+    (download url (<< "~{keyrings}/~{key}") "506ca4fb064c77a6fafc0de072d5f89076e9e9eeab27b1a84c7f0b73de4934b7")
+    (key-file (<< "~{keyrings}/~{key}"))
+    (file listing :present)
+    (line listing repo :present)
+    (update-)
+    (package "java-1.8.0-amazon-corretto-jdk:amd64" :absent)
+    (package "java-11-amazon-corretto-jdk" :present)))
