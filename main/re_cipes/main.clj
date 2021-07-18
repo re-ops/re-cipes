aa393204d732f29fec8ef9f52426c0b3bdaae103
ronen <narkisr@gmail.com>
2021-07-18 19:10:31 +1000

    Cog updated

--- main/re_cipes/main.clj
+++ main/re_cipes/main.clj
@@ -47,7 +47,7 @@
     :app {
       :command     "re-cipes"
       :description "Re-cipes provisioning cli"
-      :version     "0.3.10"
+      :version     "0.3.11"
     }
     :commands [
       {:command "provision" :short "prov"
