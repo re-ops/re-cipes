97508b968e454553735fbe2f25c3d04d67c9841e
ronen <narkisr@gmail.com>
2021-07-18 19:10:06 +1000

    Latest cog

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
       [org.clojure/clojure "1.10.3"]
-      [re-cog "0.6.1"]
+      [re-cog "0.6.4"]
   ]
 
   :plugins [
