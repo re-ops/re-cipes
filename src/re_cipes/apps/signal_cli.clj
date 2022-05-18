474a7276cfb2eae700e373f2c81deffd101cbfb2
ronen <narkisr@gmail.com>
2022-05-19 00:30:46 +1000

    adding java home and logs, fixing binary path

--- src/re_cipes/apps/signal_cli.clj
+++ src/re_cipes/apps/signal_cli.clj
@@ -3,7 +3,7 @@
   (:require
    [re-cipes.access :refer (permissions)]
    [re-cog.resources.service :refer (on-boot)]
-   [re-cog.resources.file :refer (symlink)]
+   [re-cog.resources.file :refer (symlink directory)]
    [re-cog.resources.systemd :refer (set-service)]
    [re-cog.common.recipe :refer (require-recipe)]))
 
@@ -16,21 +16,25 @@
         sum "26254ef650c1d6913d9af411a152926bbc7be6f93625cf0e1456d16b45e77b22"
         artifact (<< "signal-cli-~{version}-Linux.tar.gz")
         dest (<< "/usr/src/~{artifact}")
-        url (<< "https://github.com/AsamK/signal-cli/releases/download/v~{version}/~{artifact}")]
+        url (<< "https://github.com/AsamK/signal-cli/releases/download/v~{version}/~{artifact}")
+        bin "/usr/local/bin/signal-cli"
+        {:keys [home]} (configuration)]
     (download url dest sum)
     (untar dest "/opt/")
-    (symlink "/usr/local/bin/signal-cli" (<< "/opt/signal-cli-~{version}"))))
+    (symlink bin (<< "/opt/signal-cli-~{version}/bin/signal-cli"))
+    (chmod bin "+x" {})))
 
 (def-inline signal-cli-service
   "Setting up signal-cli service (make sure to link the device before starting it) "
   []
   (let [{:keys [user]} (configuration)
-        {:keys [number]} (configuration :signal-cli)
-        cmd (<< "/usr/local/bin/signal-cli -a ~{number} -o json daemon --socket")
+        {:keys [number log]} (configuration :signal-cli)
+        cmd (<< "/usr/local/bin/signal-cli -a ~{number} -o json daemon --socket --log-file ~{log}")
         opts {:wants "basic.target"
               :after "basic.target network.target"
               :restart "always"
               :stop "/bin/kill -HUP $MAINPID"
               :wanted-by "multi-user.target"
+              :environment {:JAVA_HOME "/usr/lib/jvm/java-17-amazon-corretto"}
               :user user}]
     (set-service "signal-cli" "Signal cli daemo process" cmd opts)))
